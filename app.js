96e674a498d3c908311f623b2b021acb69c1b2ba
vaibhav waghmare <itsmevaibhavwaghmare7666@gmail.com>
2023-08-04 20:04:42 +0530

    adding task-manager-backend v2

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 require("dotenv").config();
 app.use(bodyParser.json());
 
